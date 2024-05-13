ab698bb998551f0f18f346d91e81977899fe0bb4
Júlio Alencar <jujujilio20@gmail.com>
2024-05-13 17:35:40 -0300

    commit IN paciente

--- login/site.php
+++ login/site.php
@@ -2,6 +2,27 @@
     session_start();
     require_once "config.php";
 
+       //teste
+    //    $sql = "SELECT nome FROM users WHERE id = 1"; // Supondo que o nome está na tabela usuarios e você deseja o nome do usuário com ID 1
+
+    //    $resultado = $conn->query($sql);
+       
+    //    if ($resultado->num_rows > 0) {
+    //        // Obtém o resultado da consulta
+    //        $row = $resultado->fetch_assoc();
+    //        $nome = $row["nome"];
+       
+    //        // Retorna o nome como resposta
+    //        echo $nome;
+    //    } else {
+    //        echo "Nome não encontrado";
+    //    }
+       
+    //    // Fecha a conexão com o banco de dados
+    //    $conn->close();
+   
+       //teste acaba aqui
+
     function logout(){
         session_unset();
         session_destroy();
@@ -17,4 +38,6 @@
     if(isset($_POST["logout"])){
         logout();
     }
-    include "site.html"; ?>
+    include "site.html"; 
+    
+ 
